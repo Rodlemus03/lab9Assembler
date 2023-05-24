022a379c4291039c4369c9e36d0453004de608bb
Mauricio Julio Rodrigo Lemus Guzmán <93178012+Rodlemus03@users.noreply.github.com>
2023-05-24 08:55:47 -0600

    prueba cambios

---
+++ laboratorio9/laboratorio9/laboratorio9.asm
@@ -0,0 +1,25 @@
+;						DATOS DEL PROYECTO
+;Universidad del Valle de Guatemala
+;Mauricio Lemus 22461
+;
+;Descripcion: Ejemplo de plantilla y assembler y el uso de GPRs
+;19/04/2023
+;						HEADER
+
+.386 ;Codigo del procesador, desde el primero
+.model flat, stdcall ;Memoria
+.stack 4096 ;Deja el area de memoria libre
+
+.data
+
+
+
+.code
+main PROC 
+
+
+
+
+
+main ENDP
+END main
